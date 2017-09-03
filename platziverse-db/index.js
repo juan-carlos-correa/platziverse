7cf1950f219f738758d86de92131efe53c732562
Juan Carlos Correa <juan.correa.herrera@gmail.com>
2017-09-03 18:45:38 -0500

    databse module implementation

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -1,6 +1,19 @@
 'use strict'
 
+const setUpDatabase = require('./lib/db')
+const setUpAgentModel = require('./models/agent')
+const setUpMetricModel = require('./models/metric')
+
 module.exports = async function (config) {
+  const sequelize = setUpDatabase(config)
+  const AgentModel = setUpAgentModel(config)
+  const MetricModel = setUpMetricModel(config)
+
+  AgentModel.hasMany(MetricModel)
+  MetricModel.belongsTo(AgentModel)
+
+  await sequelize.authenticate()
+
   const Agent = {}
   const Metrict = {}
 
